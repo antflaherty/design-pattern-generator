fae5c717ca8178135dd492a56c2d0bb4a8d577c1
Gabriel Barker <gabrielbarker@gmail.com>
2020-04-30 17:39:10 +0100

    PRETTIERIFY

--- src/test/TypeScriptLanguage.test.ts
+++ src/test/TypeScriptLanguage.test.ts
@@ -1,29 +1,29 @@
-import { expect } from "chai";
-import "mocha";
-import TypeScriptLanguage from "../lib/TypeScriptLanguage";
+import { expect } from 'chai';
+import 'mocha';
+import TypeScriptLanguage from '../lib/TypeScriptLanguage';
 
 const testMethod1 = `testMethod1(): void {}`;
 const testMethod2 = `testMethod2():string { return "test"; }`;
 
-describe("Template: testMethod", () => {
-  it("should return 0", () => {
-    const codeSpec: any = {
-      name: "TestClass",
-      visibility: "public",
-    };
+describe('Template: testMethod', () => {
+	it('should return 0', () => {
+		const codeSpec: any = {
+			name: 'TestClass',
+			visibility: 'public',
+		};
 
-    const code: string = `
+		const code: string = `
     ${testMethod1}
     ${testMethod2}
     `.trim();
 
-    const expectedClass = `
+		const expectedClass = `
 ${codeSpec.visibility} class ${codeSpec.name} {
-  ${codeSpec}
+  ${code}
 }
     `.trim();
 
-    const language: TypeScriptLanguage = new TypeScriptLanguage();
-    expect(language.getClass(codeSpec, code)).to.equal(expectedClass);
-  });
+		const language: TypeScriptLanguage = new TypeScriptLanguage();
+		expect(language.getClass(codeSpec, code)).to.equal(expectedClass);
+	});
 });
