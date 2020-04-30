b2b03e46ec66d74e4a58e64e38beb9d5e0d62110
Gabriel Barker <gabrielbarker@gmail.com>
2020-04-30 17:33:22 +0100

    TEST the getClass method

---
+++ src/test/TypeScriptLanguage.test.ts
@@ -0,0 +1,29 @@
+import { expect } from "chai";
+import "mocha";
+import TypeScriptLanguage from "../lib/TypeScriptLanguage";
+
+const testMethod1 = `testMethod1(): void {}`;
+const testMethod2 = `testMethod2():string { return "test"; }`;
+
+describe("Template: testMethod", () => {
+  it("should return 0", () => {
+    const codeSpec: any = {
+      name: "TestClass",
+      visibility: "public",
+    };
+
+    const code: string = `
+    ${testMethod1}
+    ${testMethod2}
+    `.trim();
+
+    const expectedClass = `
+${codeSpec.visibility} class ${codeSpec.name} {
+  ${codeSpec}
+}
+    `.trim();
+
+    const language: TypeScriptLanguage = new TypeScriptLanguage();
+    expect(language.getClass(codeSpec, code)).to.equal(expectedClass);
+  });
+});
