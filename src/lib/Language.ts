b61359465119358196da9304efb9d6e0a4fc533e
antflaherty <antrflaherty@gmail.com>
2020-05-01 11:16:44 +0100

    Add getMethod method

--- src/lib/Language.ts
+++ src/lib/Language.ts
@@ -1,3 +1,12 @@
 export default interface Language {
 	getClass(codeSpec: { name: string; visibility: string }, code: string): string;
+	getMethod(
+		codeSpec: {
+			name: string;
+			visibility: string;
+			type: string;
+			params: { name: string; type: string }[];
+		},
+		code: string
+	): string;
 }
--- src/lib/TypeScriptLanguage.ts
+++ src/lib/TypeScriptLanguage.ts
@@ -14,6 +14,34 @@ export default class TypeScriptLanguage implements Language {
 		return classCode;
 	}
 
+	public getMethod(
+		codeSpec: {
+			name: string;
+			visibility: string;
+			type: string;
+			params: { name: string; type: string }[];
+		},
+		code: string
+	): string {
+		let methodCode = '';
+
+		if (codeSpec.visibility) {
+			methodCode = `${codeSpec.visibility} `;
+		}
+
+		methodCode += `${codeSpec.name}(`;
+
+		if (codeSpec.params) {
+			codeSpec.params.forEach((param) => {
+				methodCode += `${param.name}: ${param.type}, `;
+			});
+			methodCode = methodCode.substr(0, methodCode.length - 2);
+		}
+
+		methodCode += `) {\n${this.indentCode(code)}\n}`;
+		return methodCode;
+	}
+
 	private indentCode(code: string): string {
 		const codeLines = code.split('\n');
 		const indentedCodeLines = codeLines.map((line) => {
