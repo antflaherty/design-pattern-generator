28a714ef4ea73068961928657690d30ebf181ef2
antflaherty <antrflaherty@gmail.com>
2020-05-01 10:18:30 +0100

    Fix indentation of code within class

--- src/lib/TypeScriptLanguage.ts
+++ src/lib/TypeScriptLanguage.ts
@@ -1,12 +1,24 @@
 import Language from './Language';
 
 export default class TypeScriptLanguage implements Language {
-	getClass(codeSpec: { name: string; visibility: string }, code: string): string {
+	public getClass(codeSpec: { name: string; visibility: string }, code: string): string {
 		const classVisibilty = codeSpec.visibility;
 		const className = codeSpec.name;
-		return `
-${classVisibilty} class ${className} {
-  ${code}
-}`.trim();
+		let classCode = '';
+
+		if (classVisibilty) {
+			classCode += `${classVisibilty} `;
+		}
+		classCode += `class ${className} {\n${this.indentCode(code)}\n}`;
+
+		return classCode;
+	}
+
+	private indentCode(code: string): string {
+		const codeLines = code.split('\n');
+		const indentedCodeLines = codeLines.map((line) => {
+			return `\t${line}`;
+		});
+		return indentedCodeLines.join('\n');
 	}
 }
