736909626cf5d4869efb9b9cc3a30f7b83d312fb
Gabriel Barker <gabrielbarker@gmail.com>
2020-05-01 09:59:14 +0100

    TEST that getClass works if visibility is missing

--- src/test/TypeScriptLanguage.test.ts
+++ src/test/TypeScriptLanguage.test.ts
@@ -5,23 +5,48 @@ import TypeScriptLanguage from '../lib/TypeScriptLanguage';
 const testMethod1 = `testMethod1(): void {}`;
 const testMethod2 = `testMethod2():string { return "test"; }`;
 
-describe('Template: testMethod', () => {
-	it('should return 0', () => {
+describe('TypeScriptLanguage: getClass', () => {
+	it('should return a properly formatted public class', () => {
 		const codeSpec: any = {
 			name: 'TestClass',
 			visibility: 'public',
 		};
 
 		const code: string = `
-    ${testMethod1}
-    ${testMethod2}
-    `.trim();
+		${testMethod1}
+		${testMethod2}
+		`.trim();
 
-		const expectedClass = `
-${codeSpec.visibility} class ${codeSpec.name} {
-  ${code}
-}
-    `.trim();
+		const expectedClassLines = [
+			`${codeSpec.visibility} class ${codeSpec.name} {`,
+			`	${testMethod1}`,
+			`	${testMethod2}`,
+			`}`,
+		];
+		const expectedClass = expectedClassLines.join('\n');
+
+		const language: TypeScriptLanguage = new TypeScriptLanguage();
+		expect(language.getClass(codeSpec, code)).to.equal(expectedClass);
+	});
+
+	it('should return a properly formatted class if visibility is omitted', () => {
+		const codeSpec: any = {
+			name: 'TestClass',
+			visibility: '',
+		};
+
+		const code: string = `
+		${testMethod1}
+		${testMethod2}
+		`.trim();
+
+		const expectedClassLines = [
+			`class ${codeSpec.name} {`,
+			`	${testMethod1}`,
+			`	${testMethod2}`,
+			`}`,
+		];
+		const expectedClass = expectedClassLines.join('\n');
 
 		const language: TypeScriptLanguage = new TypeScriptLanguage();
 		expect(language.getClass(codeSpec, code)).to.equal(expectedClass);
