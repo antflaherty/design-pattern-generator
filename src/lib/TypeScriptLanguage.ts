78f500594b21d524da0f2eba1601495f9399276a
antflaherty <antrflaherty@gmail.com>
2020-05-15 09:54:48 +0100

    Extract duplicated code

--- src/lib/TypeScriptLanguage.ts
+++ src/lib/TypeScriptLanguage.ts
@@ -4,58 +4,39 @@ import { CodeSpec } from './CodeSpec';
 export default class TypeScriptLanguage implements Language {
 	public getClass(codeSpec: CodeSpec, code: string): string {
 		const { modifier, name, visibility } = codeSpec;
-		let classCode = '';
 
-		if (visibility) {
-			classCode += `${visibility} `;
-		}
-
-		if (modifier) {
-			classCode += `${modifier} `;
-		}
-
-		classCode += `class ${name} {\n${this.indentCode(code)}\n}`;
+		const classCode = this.prependVisibilityAndModifier(
+			`class ${name} {\n${this.indentCode(code)}\n}`,
+			visibility,
+			modifier
+		);
 		return classCode;
 	}
 
 	public getMethod(codeSpec: CodeSpec, code: string): string {
 		const { modifier, name, params, type, visibility } = codeSpec;
-		let methodCode = '';
 
-		if (visibility) {
-			methodCode += `${visibility} `;
-		}
-
-		if (modifier) {
-			methodCode += `${modifier} `;
-		}
-
-		methodCode += `${name}(`;
-
+		let methodCode = `${name}(`;
 		if (params) {
 			params.forEach((param) => {
 				methodCode += `${param.name}: ${param.type}, `;
 			});
 			methodCode = methodCode.substr(0, methodCode.length - 2);
 		}
-
 		methodCode += `): ${type} {\n${this.indentCode(code)}\n}`;
+
+		methodCode = this.prependVisibilityAndModifier(methodCode, visibility, modifier);
 		return methodCode;
 	}
 
 	public getVariable(codeSpec: CodeSpec): string {
 		const { visibility, modifier, name, type } = codeSpec;
 
-		let variableCode = '';
-		if (visibility) {
-			variableCode += `${visibility} `;
-		}
-
-		if (modifier) {
-			variableCode += `${modifier} `;
-		}
-
-		variableCode += `${name}: ${type};`;
+		const variableCode = this.prependVisibilityAndModifier(
+			`${name}: ${type};`,
+			modifier,
+			visibility
+		);
 		return variableCode;
 	}
 
@@ -65,5 +46,21 @@ export default class TypeScriptLanguage implements Language {
 			return `\t${line}`;
 		});
 		return indentedCodeLines.join('\n');
+	}
+
+	private prependVisibilityAndModifier(
+		code: string,
+		visibility?: string,
+		modifier?: string
+	): string {
+		if (modifier) {
+			code = `${modifier} ${code}`;
+		}
+
+		if (visibility) {
+			code = `${visibility} ${code}`;
+		}
+
+		return code;
 	}
 }
